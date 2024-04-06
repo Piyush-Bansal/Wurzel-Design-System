8f16afdf57857d16181f5f32cb32989172e969c3
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-04-06 15:34:30 +0530

    truncate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,23 @@
 	<div>
 		<p>Lorem, ipsum.</p>
 	</div>
+	<div class="truncate-3">
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus modi eveniet culpa
+		exercitationem, tenetur itaque beatae. Quis omnis id dolorum exercitationem magnam aliquam,
+		cumque, eligendi odio harum enim perspiciatis! Neque natus rerum doloribus, quod soluta
+		distinctio, esse fuga impedit minima molestiae earum, necessitatibus tempore explicabo tenetur
+		maiores adipisci deleniti! Eveniet eligendi assumenda voluptatibus a. Earum necessitatibus
+		temporibus delectus harum eaque sunt perspiciatis, dolore unde tempore ullam esse libero itaque,
+		similique error sequi consectetur possimus nisi facere voluptatem distinctio enim fuga
+		voluptates! Magni eveniet molestiae quasi minus delectus quos. Enim repudiandae blanditiis
+		voluptates at, iusto saepe suscipit tempora, quia aliquam ipsum voluptas corporis quasi,
+		repellendus rerum id quibusdam laborum rem expedita aut fugiat consequatur qui veritatis! Porro
+		eos laboriosam dolor ex aperiam esse tempora doloremque, accusantium, illo, fugit dolore et iste
+		mollitia voluptates. Ut quas explicabo quisquam voluptatum nesciunt dolorem, expedita blanditiis
+		et laudantium fugiat iusto, doloribus nobis natus? Modi facilis optio quia molestias nostrum
+		neque temporibus veniam dicta fugit ipsam aperiam voluptatibus dolores consequuntur nulla
+		officia repellendus, quos, ducimus magnam eius id. Eligendi earum sed minus praesentium ullam
+		porro aspernatur perspiciatis adipisci, eaque explicabo ab dolor possimus, ducimus non eum
+		assumenda dignissimos quod consequatur fugit? Repudiandae in sapiente nam ipsum.
+	</div>
 </div>
