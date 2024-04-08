989777cb373db1774bb9deb71b4d8355ffcde197
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-04-08 18:28:54 +0530

    vertical separator added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,14 @@
 	<h2 class="uppercase red">Hello World</h2>
 	<p><a href="">Hello</a></p>
 	<div class="separator" data-size="left-right-extended"></div>
+	<div class="tracker">
+		<div class="vertical-separator"></div>
+	</div>
 	<p>Lorem ipsum dolor sit amet.</p>
 </div>
+
+<style>
+	.tracker {
+		height: 20vh;
+	}
+</style>
