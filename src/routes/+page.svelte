9e7ae67d490447d66e0cc5b28de832ad9a9ba721
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-04-08 18:25:07 +0530

    separator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="container">
 	<h2 class="uppercase red">Hello World</h2>
 	<p><a href="">Hello</a></p>
+	<div class="separator" data-size="left-right-extended"></div>
 	<p>Lorem ipsum dolor sit amet.</p>
 </div>
