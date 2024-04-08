0d080b85848294c55f6ae85ef9c979ef395a16f9
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-04-08 16:21:52 +0530

    masonry grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="container">
-	<h2 class="uppercase">Hello World</h2>
+	<h2 class="uppercase red">Hello World</h2>
 	<p><a href="">Hello</a></p>
 	<p>Lorem ipsum dolor sit amet.</p>
 </div>
