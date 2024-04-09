e61a52d933446f56e75f819958d7fcd5677e9b8e
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-04-09 20:09:49 +0530

    divide

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,25 @@
 <div class="container">
-	<h2 class="uppercase red">Hello World</h2>
-	<p><a href="">Hello</a></p>
-	<div class="separator" data-size="left-right-extended"></div>
-	<div class="tracker">
-		<div class="vertical-separator"></div>
+	<div class="divide-y">
+		<div>
+			<p>Lorem ipsum dolor sit amet.</p>
+		</div>
+		<div>
+			<p>Lorem ipsum dolor sit amet.</p>
+		</div>
+		<div>
+			<p>Lorem ipsum dolor sit amet.</p>
+		</div>
+		<div>
+			<p>Lorem ipsum dolor sit amet.</p>
+		</div>
 	</div>
-	<p>Lorem ipsum dolor sit amet.</p>
 </div>
 
 <style>
-	.tracker {
-		height: 20vh;
+	.divide-y {
+		display: flex;
+		flex-direction: column;
+		height: 50px;
+		align-items: center;
 	}
 </style>
