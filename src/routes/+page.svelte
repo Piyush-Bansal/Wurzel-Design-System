d86a4142f7b38095cc17f1fedc58053ffd01813d
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-04-17 23:50:52 +0530

    paragraph indent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,33 @@
 <div class="container">
-	<div class="grid">
-		<div>
-			<p>Lorem ipsum dolor sit amet.</p>
-		</div>
-		<div>
-			<p>Lorem ipsum dolor sit amet.</p>
-		</div>
-		<div>
-			<p>Lorem ipsum dolor sit amet.</p>
-		</div>
-		<div>
-			<p>Lorem ipsum dolor sit amet.</p>
-		</div>
-	</div>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore beatae suscipit
+		perferendis similique, hic cumque quo deserunt eaque velit nemo assumenda cum dolor expedita
+		nulla earum consectetur error, quos modi a magnam exercitationem reprehenderit! Natus aliquam
+		quod architecto velit quia corrupti. Exercitationem minus cupiditate non cum, provident
+		voluptatum voluptate.
+	</p>
+	<p>
+		Fugit sint cumque ut non praesentium nostrum blanditiis magni inventore, consectetur iusto
+		dolorum debitis, enim est adipisci illum deleniti ratione? Officia, labore. Asperiores quas
+		ducimus commodi doloribus, nobis culpa voluptatem iusto autem at optio, suscipit consequuntur ea
+		numquam distinctio deserunt quis dolore, quae aliquid. Corporis, illum. Veniam recusandae nemo
+		accusantium!
+	</p>
+	<p>
+		Fugit sint cumque ut non praesentium nostrum blanditiis magni inventore, consectetur iusto
+		dolorum debitis, enim est adipisci illum deleniti ratione? Officia, labore. Asperiores quas
+		ducimus commodi doloribus, nobis culpa voluptatem iusto autem at optio, suscipit consequuntur ea
+		numquam distinctio deserunt quis dolore, quae aliquid. Corporis, illum. Veniam recusandae nemo
+		accusantium!
+	</p>
+	<p>
+		Fugit sint cumque ut non praesentium nostrum blanditiis magni inventore, consectetur iusto
+		dolorum debitis, enim est adipisci illum deleniti ratione? Officia, labore. Asperiores quas
+		ducimus commodi doloribus, nobis culpa voluptatem iusto autem at optio, suscipit consequuntur ea
+		numquam distinctio deserunt quis dolore, quae aliquid. Corporis, illum. Veniam recusandae nemo
+		accusantium!
+	</p>
 </div>
 
-<style>
-	.divide-y {
-		display: flex;
-		flex-direction: column;
-		height: 50px;
-		align-items: center;
-	}
+<style lang="scss">
 </style>
