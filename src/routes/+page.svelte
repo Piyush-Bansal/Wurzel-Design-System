824fbf61fd8c40a86177596fb254b82ad692c4d5
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-06-11 22:14:35 +0530

    font sizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="container">
-	<h1 class="test">Home</h1>
+	<h1 class="test p-y-128">Home</h1>
 	<p>
 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi inventore
 		beatae suscipit perferendis similique, hic cumque quo deserunt eaque velit
@@ -35,51 +35,6 @@
 </div>
 
 <style lang="scss">
-	@use 'sass:math';
-	@function calc-clamp($font-large) {
-		$font-size-small: math.div($font-large, 1920) * 1080;
+	
 
-		// Calculate the difference in font sizes
-		$font-size-difference: $font-large - $font-size-small;
-
-		// Calculate the difference in screen sizes
-		$screen-size-difference: 1920 - 1080;
-
-		// Calculate the rate of change per vw unit
-		$rate-of-change: math.div($font-size-difference, $screen-size-difference) *
-			100vw;
-
-		@debug $rate-of-change;
-
-		// Convert the small font size to rem units
-		$font-small-rem: math.div($font-size-small, 16) + rem;
-
-		// Convert the large font size to rem units
-		$font-large-rem: math.div($font-large, 16) + rem;
-
-		// Calculate the px adjustment
-		$px-adjustment: ($rate-of-change * math.div(1920, 100vw)) - $font-large;
-
-		@debug $px-adjustment;
-
-		// Convert the px adjustment to rem
-		$rem-adjustment: math.div($px-adjustment, 16) + rem;
-
-		// Return the clamp expression
-		@return clamp(
-			#{$font-small-rem},
-			#{$rate-of-change + ' - ' + $rem-adjustment},
-			#{$font-large-rem}
-		);
-	}
-
-	// Example usage:
-	// body {
-	//   font-size: calc-clamp(20, 30, 1080, 1920);
-	// }
-
-	.test {
-		font-size: calc-clamp(30);
-		padding: var(--size-96);
-	}
 </style>
