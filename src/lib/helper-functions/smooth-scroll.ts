f8927279b044f5c8cc689e05cd662f49e8448305
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-07-08 16:15:51 +0530

    lenis scroll and gsap added

---
+++ src/lib/helper-functions/smooth-scroll.ts
@@ -0,0 +1,32 @@
+import Lenis from 'lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+export const smooth = function () {
+	const lenis = new Lenis({
+		lerp: 0.1,
+		wheelMultiplier: 3,
+		infinite: false,
+		gestureOrientation: 'vertical',
+		normalizeWheel: false,
+		smoothTouch: false
+	});
+
+	function raf(time) {
+		lenis.raf(time);
+		requestAnimationFrame(raf);
+	}
+
+	requestAnimationFrame(raf);
+
+	const connectToScrollTrigger = function () {
+		lenis.on('scroll', ScrollTrigger.update);
+
+		gsap.ticker.add((time) => {
+			lenis.raf(time * 1000);
+		});
+
+		gsap.ticker.lagSmoothing(0);
+	};
+	connectToScrollTrigger();
+};
