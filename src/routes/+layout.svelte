6bb94b03a2a6e0440f663269f3fc31fbf95af94d
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-07-21 16:04:42 +0530

    template update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import '$styles/main.scss';
+	import { smooth } from '$lib/helper-functions/smooth-scroll';
+	import { browser } from '$app/environment';
+
+	//enable smooth scroll
+	if (browser) {
+		smooth();
+	}
 
 	let { children } = $props();
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,56 +2,9 @@
 	import { smooth } from '$lib/helper-functions/smooth-scroll';
 	import { browser } from '$app/environment';
 	import { Accordion, AccordionItem } from '$components/accordion';
-
-	if (browser) {
-		smooth();
-	}
-
-	const accordionData = [
-		{
-			heading: 'About Us',
-			description:
-				'We are a team of passionate developers dedicated to delivering high-quality software solutions.'
-		},
-		{
-			heading: 'Services',
-			description:
-				'We offer a range of services including web development, mobile app development, and UI/UX design.'
-		},
-		{
-			heading: 'Contact Us',
-			description:
-				'Feel free to reach out to us with any questions or inquiries. We will get back to you as soon as possible.'
-		}
-	];
 </script>
 
-<div class="container | flow grid">
-	<Accordion collapsable={false}>
-		{#each accordionData as data, i}
-			<AccordionItem open={i === 0} group="a">
-				{#snippet heading()}
-					{data.heading}
-				{/snippet}
-				{#snippet description()}
-					{data.description}
-				{/snippet}
-			</AccordionItem>
-		{/each}
-	</Accordion>
-	<Accordion collapsable={true}>
-		{#each accordionData as data, i}
-			<AccordionItem open={i === 0} group="b">
-				{#snippet heading()}
-					{data.heading}
-				{/snippet}
-				{#snippet description()}
-					{data.description}
-				{/snippet}
-			</AccordionItem>
-		{/each}
-	</Accordion>
-</div>
+<div class="container"></div>
 
 <style lang="scss">
 </style>
