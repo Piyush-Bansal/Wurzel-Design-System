34761204233dfdde48b2dc8ad641749b2e95bc5f
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2024-07-26 16:22:10 +0530

    Carousel files

---
+++ src/components/carousel/index.ts
@@ -0,0 +1,6 @@
+export { default as Carousel } from './Carousel.svelte';
+export { default as CarouselContent } from './CarouselContent.svelte';
+export { default as CarouselItem } from './CarouselItem.svelte';
+export { default as Next } from './Next.svelte';
+export { default as Previous } from './Previous.svelte';
+export { default as Indicator } from './Indicator.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,58 @@
 <script lang="ts">
-	import { TabList, Tabs, TabTrigger, TabContent } from '$components/tab';
+	import { Tab, TabList, TabTrigger, TabContent } from '$components/tab';
+	const data = [
+		{
+			id: 1,
+			url: 'https://picsum.photos/800'
+		},
+		{
+			id: 2,
+			url: 'https://picsum.photos/800'
+		},
+		{
+			id: 3,
+			url: 'https://picsum.photos/800'
+		},
+		{
+			id: 4,
+			url: 'https://picsum.photos/800'
+		},
+		{
+			id: 5,
+			url: 'https://picsum.photos/800'
+		},
+		{
+			id: 6,
+			url: 'https://picsum.photos/800'
+		}
+	];
 </script>
 
 <div class="container">
-	<Tabs activeTab="tab1">
+	<Tab activeTab="tab1">
 		{#snippet tabList()}
 			<TabList>
 				<TabTrigger value="tab1">Tab 1</TabTrigger>
-				<TabTrigger value="tab2">Tab 22222222222222222</TabTrigger>
+				<TabTrigger value="tab2">Tab 2</TabTrigger>
 			</TabList>
 		{/snippet}
 		{#snippet tabContent()}
 			<TabContent value="tab1">Content for Tab 1</TabContent>
 			<TabContent value="tab2">Content for Tab 2</TabContent>
 		{/snippet}
-	</Tabs>
+	</Tab>
+	<Tab activeTab="tab1">
+		{#snippet tabList()}
+			<TabList>
+				<TabTrigger value="tab1">Tab 1</TabTrigger>
+				<TabTrigger value="tab2">Tab 2</TabTrigger>
+			</TabList>
+		{/snippet}
+		{#snippet tabContent()}
+			<TabContent value="tab1">Content for Tab 1</TabContent>
+			<TabContent value="tab2">Content for Tab 2</TabContent>
+		{/snippet}
+	</Tab>
 </div>
 
 <style lang="scss">
