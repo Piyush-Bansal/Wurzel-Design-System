5f2da6edcd8ba338f044324e3c5cf529c9dc99a7
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2025-01-07 14:50:30 +0530

    purge css implemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		pluginPurgeCss({
+			content: ['./src/**/*.{html,js,svelte,ts}'],
+			fontFace: true,
+			keyframes: true,
+			variables: true
+		})
+	]
 
 	// css: {
 	// 	preprocessorOptions: {
