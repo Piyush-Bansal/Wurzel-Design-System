417dd1a217c1c2c85b847ad16215b691862abfa8
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2025-04-25 12:10:26 +0530

    breakpoint change

--- src/lib/helper-functions/breakpoints-store.svelte.ts
+++ src/lib/helper-functions/breakpoints-store.svelte.ts
@@ -1,6 +1,6 @@
-export const ssLargeHigh = 1928;
+export const ssLargeHigh = 1936;
 export const ssLargeLow = 1024;
 export const ssMediumHigh = 1023;
-export const ssMediumLow = 768;
-export const ssSmallHigh = 767;
-export const ssSmallLow = 356;
+export const ssMediumLow = 701;
+export const ssSmallHigh = 700;
+export const ssSmallLow = 360;
