791cd06a9af6eaa496629eda86fb5e3b1f8cf400
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2026-05-07 18:33:28 +0530

    aspect ratio working and purge not working

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 import postcssPresetEnv from 'postcss-preset-env';
 // import purgecss from '@fullhuman/postcss-purgecss';
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5';
 
 /** @type {import('postcss-preset-env').Config} */
 
@@ -7,19 +8,19 @@ const config = {
 	plugins: [
 		postcssPresetEnv({
 			stage: 3
+		}),
+		pluginPurgeCss({
+			content: ['./src/**/*.{html,js,svelte,ts}'],
+			safelist: [/svelte-/, /^:global/],
+			defaultExtractor: (content) => {
+				const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+				const innerMatches =
+					content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+				return broadMatches.concat(innerMatches);
+			},
+			// Add SCSS files to be processed
+			dynamicAttributes: ['class', 'class:list']
 		})
-		// purgecss({
-		// 	content: ['./src/**/*.{html,js,svelte,ts}'],
-		// 	safelist: [/svelte-/, /^:global/],
-		// 	defaultExtractor: (content) => {
-		// 		const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-		// 		const innerMatches =
-		// 			content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-		// 		return broadMatches.concat(innerMatches);
-		// 	},
-		// 	// Add SCSS files to be processed
-		// 	dynamicAttributes: ['class', 'class:list']
-		// })
 	]
 };
 
