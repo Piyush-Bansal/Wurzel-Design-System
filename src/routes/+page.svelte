816c95d6492e30e8316395054cff4c4a5a2f4186
Piyush-Bansal <39044544+Piyush-Bansal@users.noreply.github.com>
2026-05-20 15:08:36 +0530

    string replacement function added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
 	import { Accordion, AccordionItem } from '$components/accordion';
-	import Button from '$components/button/Button.svelte';
-	import Countdown from '$components/countdown/Countdown.svelte';
-	import { Tab, TabContent, TabList, TabTrigger } from '$components/tab';
-	import {
-		Carousel,
-		CarouselContent,
-		CarouselItem
-	} from '$components/carousel';
 	import Slide from '$components/tab/Slide.svelte';
 	const data = [Slide, Slide, Slide, Slide, Slide, Slide, Slide, Slide];
 
@@ -27,7 +19,7 @@
 	];
 </script>
 
-<div class="container">
+<div class="container | paragraph-indent">
 	<p class="uppercase">
 		im obcaecati natus explicabo expedita? Tenetur perspiciatis aliquam esse non
 		culpa expedita possimus. Repudiandae corporis obcaecati aliquid eum
@@ -50,33 +42,6 @@
 	</p>
 	<a href="/">Hello</a>
 
-	<!-- <Countdown
-		targetDate="2025-12-16T23:01:00"
-		options={{ onComplete: () => console.log('YAY!') }}
-	/> -->
-
-	<!-- <Tab activeTab="tab1">
-		{#snippet tabList()}
-			<TabList label="tablist">
-				<TabTrigger value="tab1">Tab 1</TabTrigger>
-				<TabTrigger value="tab2">Tab 2</TabTrigger>
-			</TabList>
-		{/snippet}
-		{#snippet tabContent()}
-			<TabContent value="tab1"
-				>im obcaecati natus explicabo expedita? Tenetur perspiciatis aliquam
-				esse non culpa expedita possimus. Repudiandae corporis obcaecati aliquid
-				eum molestias id delectus nihil provident accusantium excepturi quo nemo
-				maxime dicta hic similique veritatis quod, vel aliquam! Labore
-				reprehenderit blanditiis, est dolores illo quos dolor esse laudantium
-				officia distinctio veritatis saepe totam qui voluptatem! Suscipit modi
-				qui officia delectus maxime, nam sequi deleniti sapiente est omnis ad
-				iure saepe quis. Similique, non!</TabContent
-			>
-			<TabContent value="tab2">Content for Tab 2</TabContent>
-		{/snippet}
-	</Tab> -->
-
 	<Accordion collapsable={true}>
 		{#each accordionData as data, i}
 			<AccordionItem open={i === 0} group="b">
@@ -90,34 +55,3 @@
 		{/each}
 	</Accordion>
 </div>
-
-<!-- <div class="container">
-	<Button variant="primary" onclick={() => console.log('clicked')}
-		><p>Primary</p></Button
-	>
-</div> -->
-
-<!-- <div class="container" data-size="full">
-	<Carousel
-		autoscroll={false}
-		indicator={true}
-		controls={true}
-		infinite={false}
-		count={data.length}
-	>
-		<CarouselContent
-			size={{ lg: 589, md: 344, sm: 324 }}
-			leftPadding={{ lg: 64, md: 32, sm: 16 }}
-		>
-			{#each data as slide, number}
-				<CarouselItem>
-					<Slide number={number + 1}></Slide>
-				</CarouselItem>
-			{/each}
-		</CarouselContent>
-	</Carousel>
-</div>
-
-<p class="p-28">
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, aperiam.
-</p> -->
